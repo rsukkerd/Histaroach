7901696189008a343f209c9eb58ecca025bcfca6
Ivan Beschastnikh <ivan@cs.washington.edu>
2012-01-23 14:21:46 -0800

    Added plume lib to classpath, added a short help option as a test.

--- src/TestIsolationDataGenerator.java
+++ src/TestIsolationDataGenerator.java
@@ -1,9 +1,21 @@
-
 import java.io.IOException;
 
 import common.DataExtractor;
 
+import plume.Option;
+import plume.OptionGroup;
+import plume.Options;
+
 public class TestIsolationDataGenerator {
+
+    @OptionGroup("General Options")
+    @Option(value = "-h Print short usage message", aliases = { "-help" })
+    public static boolean help = false;
+
+    private static Options plumeOptions;
+
+    /** One line synopsis of usage */
+    public static final String usage_string = "TestIsolationDataGenerator [options]";
 
     /**
      * @param args
@@ -17,6 +29,16 @@ public class TestIsolationDataGenerator {
      * @throws IOException
      */
     public static void main(String[] args) throws IOException {
+        plumeOptions = new Options(TestIsolationDataGenerator.usage_string);
+        @SuppressWarnings("unused")
+        String[] cmdLineArgs = plumeOptions.parse_or_usage(args);
+
+        // Display help just for the 'publicized' option groups
+        if (TestIsolationDataGenerator.help) {
+            plumeOptions.print_usage();
+            return;
+        }
+
         DataExtractor.extractData(args[0], args[1], args[2], args[3]);
     }
 
