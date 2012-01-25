a0bfe1692a3d39b5087ea8b4f3f897cac0d2009a
Ivan Beschastnikh <ivan@cs.washington.edu>
2012-01-24 22:42:01 -0800

    Resolved issue #3.

--- src/TestIsolationDataGenerator.java
+++ src/TestIsolationDataGenerator.java
@@ -8,9 +8,37 @@ import plume.Options;
 
 public class TestIsolationDataGenerator {
 
+    /**
+     * Print the short usage message.
+     */
     @OptionGroup("General Options")
     @Option(value = "-h Print short usage message", aliases = { "-help" })
-    public static boolean help = false;
+    public static boolean showHelp = false;
+
+    /**
+     * Full path to the output file.
+     */
+    @Option(value = "-o File to use for output.", aliases = { "-outputfile" })
+    public static String outputFilename = null;
+
+    /**
+     * The commit ID from which to begin the analysis.
+     */
+    @Option(value = "-s Starting commit id")
+    public static String startCommitID = null;
+
+    /**
+     * The commit ID where the analysis should terminate.
+     */
+    @Option(value = "-e End commit id")
+    public static String endCommitID = null;
+
+    /**
+     * Full path to the repository directory.
+     */
+    @Option(value = "-r Full path to the repository directory.",
+            aliases = { "-repodir" })
+    public static String repositoryDir = null;
 
     private static Options plumeOptions;
 
@@ -18,14 +46,11 @@ public class TestIsolationDataGenerator {
     public static final String usage_string = "TestIsolationDataGenerator [options]";
 
     /**
+     * Initial program entrance -- parses the arguments and runs the data
+     * extraction.
+     * 
      * @param args
-     *            [0] : full path to the repository directory
-     * @param args
-     *            [1] : full path to the output file
-     * @param args
-     *            [2] : starting commit id
-     * @param args
-     *            [3] : ending commit id
+     *            command line arguments.
      * @throws IOException
      */
     public static void main(String[] args) throws IOException {
@@ -33,13 +58,13 @@ public class TestIsolationDataGenerator {
         @SuppressWarnings("unused")
         String[] cmdLineArgs = plumeOptions.parse_or_usage(args);
 
-        // Display help just for the 'publicized' option groups
-        if (TestIsolationDataGenerator.help) {
+        // Display the help screen.
+        if (showHelp) {
             plumeOptions.print_usage();
             return;
         }
 
-        DataExtractor.extractData(args[0], args[1], args[2], args[3]);
+        DataExtractor.extractData(repositoryDir, outputFilename, startCommitID,
+                endCommitID);
     }
-
 }
