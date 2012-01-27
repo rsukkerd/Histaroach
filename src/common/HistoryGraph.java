a1bfe0310bed0815e77641b893bed4e7e2d07799
Roykrong Sukkerd <rsukkerd@cs.washington.edu>
2012-01-26 20:22:29 -0800

    added more comments to HistoryGraph

--- src/common/HistoryGraph.java
+++ src/common/HistoryGraph.java
@@ -6,17 +6,27 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * HistoryGraph represents a graph structure of a particular repository.
+ */
 public class HistoryGraph {
+	/** mapping : a revision's commit id -> that revision **/
 	private final Map<String, Revision> map;
 	
 	public HistoryGraph() {
 		map = new HashMap<String, Revision>();
 	}
 	
+	/**
+	 * add a revision to this history graph
+	 */
 	public void addRevision(Revision revision) {
 		map.put(revision.getCommitID(), revision);
 	}
 	
+	/**
+	 * @return a set of parent revisions of a given child revision
+	 */
 	public Set<Revision> getParents(Revision child) {
 		Set<String> parentIDs = child.getParentIDs();
 		Set<Revision> parents = new HashSet<Revision>();
@@ -29,10 +39,16 @@ public class HistoryGraph {
 		return parents;
 	}
 	
+	/**
+	 * @return an iterator over all revisions in this history graph
+	 */
 	public Iterator<Revision> getRevisionIterator() {
 		return map.values().iterator();
 	}
 	
+	/**
+	 * @return true iff revision_A and revision_B are parallel in this history graph
+	 */
 	public boolean parallel(Revision revision_A, Revision revision_B) {
 		return !revision_A.equals(revision_B) && !isAncestorOf(revision_A, revision_B) 
 				&& !isAncestorOf(revision_B, revision_A);
@@ -68,5 +84,31 @@ public class HistoryGraph {
         }
 
         return false;
+    }
+    
+    @Override
+    public boolean equals(Object other) {
+        if (other == null || !other.getClass().equals(this.getClass())) {
+            return false;
+        }
+
+        HistoryGraph hGraph = (HistoryGraph) other;
+
+        return map.equals(hGraph.map);
+    }
+    
+    @Override
+    public int hashCode() {
+    	return map.hashCode();
+    }
+    
+    @Override
+    public String toString() {
+    	String str = "";
+    	for (Revision revision : map.values()) {
+    		str += revision.toString() + "\n";
+    	}
+    	
+    	return str;
     }
 }
