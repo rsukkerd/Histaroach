bdc2f914c8a66bdf490167533c450e65b05e7c09
Roykrong Sukkerd <rsukkerd@cs.washington.edu>
2012-04-25 23:45:26 -0700

    TestIsolationDataReader handles MixedRevision analysis; added output/mixed_revision_templates.ser

--- src/TestIsolationDataReader.java
+++ src/TestIsolationDataReader.java
@@ -1,19 +1,22 @@
 import git.GitRepository;
 
 import java.io.File;
-
-import ant.JodatimeBuildStrateygy;
-import ant.VoldemortBuildStrategy;
+import java.util.List;
 
 import plume.Option;
 import plume.OptionGroup;
 import plume.Options;
+import util.HistoryGraphXMLReader;
+import util.Util;
+import ant.JodatimeBuildStrateygy;
+import ant.VoldemortBuildStrategy;
 
 import common.BuildStrategy;
 import common.HistoryGraph;
-import common.MixingTool;
+import common.MixedRevisionTemplate;
+import common.MixedRevisionTemplatesGenerator;
+import common.MixedRevisionsGenerator;
 import common.Repository;
-import common.Util;
 
 public class TestIsolationDataReader {
 
@@ -28,35 +31,50 @@ public class TestIsolationDataReader {
     /**
 	 * Project name.
 	 */
-	@Option(value = "-p Project name (Required)",
+	@Option(value = "-p Project name",
 	        aliases = { "-projName" })
 	public static String projName = null;
 
 	/**
-     * Full path to the directory containing serialized revision files.
+     * HistoryGraph xml file.
      */
-    @Option(value = "-z Directory containing serialized revision files (Required)", 
-    		aliases = { "-serializedRevisionsDir" })
-    public static String serializedRevisionsDirName = null;
+    @Option(value = "-x HistoryGraph xml file", 
+    		aliases = { "-hGraphXML" })
+    public static String hGraphXML = null;
+    
+    /**
+     * Start index of MixedRevision to begin analysis.
+     */
+    @Option(value = "-s Start index of MixedRevision to begin analysis", 
+    		aliases = { "-startIndex" })
+    public static int startIndex = 0;
+    
+    /**
+     * Number of MixedRevisions to analysis.
+     */
+    @Option(value = "-n Number of MixedRevisions to analysis", 
+    		aliases = { "-numMixedRevisions" })
+    public static int numMixedRevisions = 0;
     
     /**
 	 * Full path to the repository directory.
 	 */
-	@Option(value = "-r Full path to the repository directory (Required)",
+	@Option(value = "-r Full path to the repository directory",
 	        aliases = { "-repoPath" })
 	public static String repoPath = null;
 	
 	/**
 	 * Full path to the cloned repository directory.
 	 */
-	@Option(value = "-c Full path to the cloned repository directory (Required)",
+	@Option(value = "-c Full path to the cloned repository directory",
 	        aliases = { "-clonedRepoPath" })
 	public static String clonedRepoPath = null;
 
 	/**
 	 * Build command. Default is 'ant'.
 	 */
-	@Option(value = "-b build command (Optional)", aliases = { "-buildCommand" })
+	@Option(value = "-b build command (Optional)", 
+			aliases = { "-buildCommand" })
 	public static String buildCommand = "ant";
 
 	/** One line synopsis of usage */
@@ -79,36 +97,53 @@ public class TestIsolationDataReader {
 	        return;
 	    }
 	    
-	    if (projName == null || serializedRevisionsDirName == null 
-	    		|| repoPath == null || clonedRepoPath == null) {
-	    	plumeOptions.print_usage();
-	    	return;
+	    if (numMixedRevisions > 0) {
+	    	
+	    	if (projName == null || repoPath == null || clonedRepoPath == null) {
+		    	plumeOptions.print_usage();
+		    	return;
+		    }
+	    	
+	    	File repoDir = new File(repoPath);
+		    File clonedRepoDir = new File(clonedRepoPath);
+		    
+		    BuildStrategy buildStrategy = null;
+	        BuildStrategy clonedBuildStrategy = null;
+	        
+	        if (projName.equals(TestIsolationDataGenerator.VOLDEMORT)) {
+	        	buildStrategy = new VoldemortBuildStrategy(repoDir, buildCommand);
+	        	clonedBuildStrategy = new VoldemortBuildStrategy(clonedRepoDir, buildCommand);
+	        } else if (projName.equals(TestIsolationDataGenerator.JODA_TIME)) {
+	        	buildStrategy = new JodatimeBuildStrateygy(repoDir, buildCommand);
+	        	clonedBuildStrategy = new JodatimeBuildStrateygy(clonedRepoDir, buildCommand);
+	        }
+	        
+	        assert buildStrategy != null && clonedBuildStrategy != null;
+	        
+	        Repository repository = new GitRepository(repoDir, buildStrategy);
+	        Repository clonedRepository = new GitRepository(clonedRepoDir, clonedBuildStrategy);
+		    
+	    	List<MixedRevisionTemplate> mixedRevisionTemplates = Util.readMixedRevisionTemplates();
+	    	
+	    	MixedRevisionsGenerator generator = 
+	    		new MixedRevisionsGenerator(mixedRevisionTemplates, repository, clonedRepository);
+	    	
+	    	generator.constructSimpleMixedRevisions(startIndex, numMixedRevisions);
+	    } else {
+	    	
+	    	if (hGraphXML == null) {
+		    	plumeOptions.print_usage();
+		    	return;
+		    }
+	    	
+	    	HistoryGraphXMLReader reader = new HistoryGraphXMLReader(new File(hGraphXML));
+		    HistoryGraph historyGraph = reader.reconstructHistoryGraph();
+		    
+		    MixedRevisionTemplatesGenerator templatesGenerator = 
+		    	new MixedRevisionTemplatesGenerator(historyGraph);
+		    
+		    templatesGenerator.generateMixedRevisionTemplates();
+		    templatesGenerator.writeOutMixedRevisionTemplates();
 	    }
-	    
-	    File repoDir = new File(repoPath);
-	    File clonedRepoDir = new File(clonedRepoPath);
-                
-        BuildStrategy buildStrategy = null;
-        BuildStrategy clonedBuildStrategy = null;
-        
-        if (projName.equals(TestIsolationDataGenerator.VOLDEMORT)) {
-        	buildStrategy = new VoldemortBuildStrategy(repoDir, buildCommand);
-        	clonedBuildStrategy = new VoldemortBuildStrategy(clonedRepoDir, buildCommand);
-        } else if (projName.equals(TestIsolationDataGenerator.JODA_TIME)) {
-        	buildStrategy = new JodatimeBuildStrateygy(repoDir, buildCommand);
-        	clonedBuildStrategy = new JodatimeBuildStrateygy(clonedRepoDir, buildCommand);
-        }
-        
-        assert buildStrategy != null && clonedBuildStrategy != null;
-        
-        Repository repository = new GitRepository(repoDir, buildStrategy);
-        Repository clonedRepository = new GitRepository(clonedRepoDir, clonedBuildStrategy);
-	    
-	    File serializedRevisionsDir = new File(serializedRevisionsDirName);
-	    
-	    HistoryGraph historyGraph = Util.reconstructHistoryGraph(serializedRevisionsDir);
-	    	    
-	    MixingTool mixing = new MixingTool(historyGraph, repository, clonedRepository);
-	    mixing.run();
 	}
 }
