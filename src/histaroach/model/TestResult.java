5fa15c3b9240991d0dc11096b10b9f64002d9689
Roykrong Sukkerd <rsukkerd@cs.washington.edu>
2012-05-05 22:29:15 -0700

    removed passedTests field and getPassedTests() method; added encodeAsString(test) method

--- src/histaroach/model/TestResult.java
+++ src/histaroach/model/TestResult.java
@@ -1,19 +1,11 @@
 package histaroach.model;
 
 import java.io.Serializable;
-import java.util.HashSet;
 import java.util.Set;
 
 
 /**
- * TestResult represents test results of a Revision. 
- * 
- * TestResult contains the following public methods: 
- *  - getAllTests(): returns a set of all tests 
- *  - getFailedTests(): returns a set of failed tests 
- *  - getPassedTests(): returns a set of passed tests 
- *  - pass(test): returns whether a test passes
- *  - fail(test): returns whether a test fails.
+ * TestResult represents results of all tests run on a Revision. 
  * 
  * TestResult is immutable.
  */
@@ -23,73 +15,53 @@ public class TestResult implements Serializable {
 	 */
 	private static final long serialVersionUID = 2210640650733708242L;
 	
-    private final /*@Non-Null*/ Set<String> allTests;
-    private final /*@Non-Null*/ Set<String> failedTests;
-    private final /*@Non-Null*/ Set<String> passedTests;
+	private static final String TRUE = "1";
+	private static final String FALSE = "0";
+	private static final String NEITHER = "-1";
+	
+    private final Set<String> allTests;
+    private final Set<String> failedTests;
 
     /**
      * Creates a TestResult.
      * 
-     * @precondition allTests and failedTests are Non-Null.
+     * @requires failedTests is a subset of allTests.
      */
-    public TestResult(/*@Non-Null*/ Set<String> allTests, 
-    		/*@Non-Null*/ Set<String> failedTests) {
+    public TestResult(Set<String> allTests, Set<String> failedTests) {
         this.allTests = allTests;
         this.failedTests = failedTests;
-        
-        passedTests = new HashSet<String>();
-        for (String test : allTests) {
-        	if (!failedTests.contains(test)) {
-        		passedTests.add(test);
-        	}
-        }
     }
     
-    /**
-     * Returns a set of all tests.
-     * 
-     * @return a set of all tests.
-     */
     public Set<String> getAllTests() {
         return allTests;
     }
-
-    /**
-     * Returns a set of failed tests.
-     * 
-     * @return a set of failed tests.
-     */
+    
     public Set<String> getFailedTests() {
         return failedTests;
     }
     
-    /**
-     * Returns a set of passed tests.
-     * 
-     * @return a set of passed tests.
-     */
-    public Set<String> getPassedTests() {
-        return passedTests;
-    }
-
-    /**
-     * Returns true if a test passes.
-     * 
-     * @return true if the test passes.
-     */
     public boolean pass(String test) {
-        return passedTests.contains(test);
+        return allTests.contains(test) && !failedTests.contains(test);
     }
-
-    /**
-     * Returns true if a test fails.
-     * 
-     * @return true if the test fails.
-     */
+    
     public boolean fail(String test) {
         return failedTests.contains(test);
     }
-
+    
+    /**
+     * Encode the result of test as a String.
+     * 
+     * @return a String encoded the result of test: 
+     *         1 means pass, 0 means fail, and -1 means no result.
+     */
+    public String encodeAsString(String test) {
+    	if (!allTests.contains(test)) {
+    		return NEITHER;
+    	}
+    	
+    	return failedTests.contains(test) ? FALSE : TRUE;
+    }
+    
     @Override
     public boolean equals(Object other) {
         if (other == null || !other.getClass().equals(this.getClass())) {
