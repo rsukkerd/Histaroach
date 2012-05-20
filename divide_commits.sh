da1cbfe374a49e4db938fb3ec915153374c50f0f
Roykrong Sukkerd <rsukkerd@cs.washington.edu>
2012-05-20 13:02:38 -0700

    modified divide_commits.sh to check out master branch (in clean state) of the repository, and use --date-order to get revision history log

--- divide_commits.sh
+++ divide_commits.sh
@@ -4,6 +4,8 @@ then
 	exit 1
 fi
 cd $1
+git checkout -- ./
+git checkout master
 lines=`git log --oneline | wc -l`
 size=$2
 i=0
@@ -13,12 +15,12 @@ do
 	start=$[$i + 1]
 	i=$[$i + $size]
 	end=$i
-	startID=`git log --pretty=format:%h | sed -n ${start}p`
-	endID=`git log --pretty=format:%h | sed -n ${end}p`
+	startID=`git log --pretty=format:%h --date-order | sed -n ${start}p`
+	endID=`git log --pretty=format:%h --date-order | sed -n ${end}p`
 	echo -s $startID -e $endID
 done
 start=$[$i+1]
 end=$lines
-startID=`git log --pretty=format:%h | sed -n ${start}p`
-endID=`git log --pretty=format:%h | sed -n ${end}p`
+startID=`git log --pretty=format:%h --date-order | sed -n ${start}p`
+endID=`git log --pretty=format:%h --date-order | sed -n ${end}p`
 echo -s $startID -e $endID
